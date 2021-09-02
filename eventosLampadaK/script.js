5cebda4f4fc5acf7ae829d49a967dbc78dd741f8
KalianePemi <kapereir@student.42sp.org.br>
2021-09-02 12:24:24 -0300

    acender lampada

---
+++ eventosLampadaK/script.js
@@ -0,0 +1,8 @@
+//document: objeto que referencia a pagina html
+//getElementById: propriedade de captura o id da pagina
+//src: carrega imagem no javascript - exemplo: src = '/img/
+var lamp = document.getElementById('lamp');
+function quebrarLampada()
+{
+    lamp.src="./img/lampadaQuebrada.jpeg"
+}
